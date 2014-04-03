db5208e03becff1810143dfaa35305d7cb96e726
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2014-04-03 15:24:57 +0500

    Moved to command-line based execution

--- MathOperations/src/com/nms/math/Calculator.java
+++ MathOperations/src/com/nms/math/Calculator.java
@@ -1,24 +1,17 @@
 package com.nms.math;
 
-import java.io.Console;
-
 public class Calculator {
 
 	public static void main(String[] args) {
 
-		System.out.println("Enter an operand\n");
-		Console console = System.console();
-		int num1 = Integer.valueOf(console.readLine());
+		int num1 = Integer.valueOf(args[0]);
+		String sign = args[1];
+		int num2 = Integer.valueOf(args[2]);
 
-		System.out.println("Enter another operand\n");
-		int num2 = Integer.valueOf(console.readLine());
-
-		System.out.println("Select an operation +\n");
-		String sign = console.readLine();
 		ArithmeticOperation operation = ArithmeticOperationsFactory.getInstance(sign);
+		int result = operation.calculate(num1, num2);
 
-		System.out.println("************************\n");
-		System.out.printf("The result is %d", operation.calculate(num1, num2));
+		System.out.printf("%d %s %d is %d", num1, sign, num2, result);
 
 	}
 
