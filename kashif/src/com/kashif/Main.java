c376bddd44be0c5712f2fa2f9a2d9073781e2fd8
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2014-04-08 12:33:09 +0500

    Long string

--- kashif/src/com/kashif/Main.java
+++ kashif/src/com/kashif/Main.java
@@ -2,6 +2,6 @@ package com.kashif;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Kashif Nazar");
+		System.out.println("Kashif Nazar, I am nice man");
 	}
 }
