0523082195d5ee625da61ea4238abcc5ea2cb0c2
ehtashamul.haq@numetrics.corp <ehtashamul.haq@numetrics.com>
2014-09-03 14:54:39 +0500

    removed
    removed

--- MathOperations/src/com/nms/math/ArithmeticOperation.java
+++
@@ -1,7 +0,0 @@
-package com.nms.math;
-
-public interface ArithmeticOperation {
-
-	public int calculate(int num1, int num2);
-
-}
--- kashif/src/com/kashif/Main.java
+++
@@ -1,8 +0,0 @@
-package com.kashif;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Kashif Nazar, I am nice man");
-	}
-}
-//End of the Main class 
